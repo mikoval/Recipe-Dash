8ce048e005b430ee8c68f098c9c53d3ac6bd8d98
Justina <ampofo_justina@yahoo.com>
2017-10-14 18:46:51 -0400

    added departments to sidebar

--- public/initPage.js
+++ public/initPage.js
@@ -1,2 +1,20 @@
-console.log("init page")
+$(document).ready(function () {
+    console.log("init page")
+
+
+    function createSidebar() {
+        var departments = getDeptNames();
+
+        for (i = 0; i < departments.length; i++) {
+            var sidebarItem = document.createElement("div");
+            sidebarItem.innerHTML = departments[i].name;
+            document.getElementById("sidebar").appendChild(sidebarItem);
+        }
+    }
+
+    createSidebar();
+});
+
+
+
 
