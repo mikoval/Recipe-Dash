761e2f8efd7a1b2a63a248faaad2657446b5c5e2
Justina <ampofo_justina@yahoo.com>
2017-10-15 02:12:11 -0400

    added cart

--- public/initPage.js
+++ public/initPage.js
@@ -1,15 +1,20 @@
 
 $(document).ready(function () {
     $("#search-btn").on("click", function(){
-    	var url = "/url?url="+ $("#search").val();
-    	$.ajax({
-    		url: url,
-    		success: function(response){
-    			response = JSON.parse(response);
-    			addIngredients(response);
-    		}
-    	})
+        location = "/cart?url="+ $("#search").val();
+    });
 
+    $("#searchMore-btn").on("click", function(){
+        var url = "/url?url="+ $("#searchMore").val();
+        $.ajax({
+            url: url,
+            success: function(response){
+                response = JSON.parse(response);
+                addIngredients(response);
+            }
+        })
     })
+    console.log(window.location.pathname.split("?"));
+
 });
 
--- server.js
+++ server.js
@@ -19,24 +19,25 @@ var server = app.listen(port, function() {
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function (req, resOrigin) {
-	var output = "";
-	var options = {
-	  host: 'hackgt-api.ncrcloud.com',
-	  port: 443,
-	  path: '/catalog/items/snapshot',
-	  method: 'GET',
-	  headers: {
-	  	"Authorization" : "Basic L29yZy0xL2FkbWluOkNoYW5nM20zISEtYWRtaW4tb3JnLTE=",
-	    "nep-application-key" : "8a82859f5ef21870015ef2fa5e5f0000",
-    	"Content-Type": "application/json",
-	  }
+
+  app.get('/', function (req, resOrigin) {
+    var output = "";
+    var options = {
+      host: 'hackgt-api.ncrcloud.com',
+      port: 443,
+      path: '/catalog/items/snapshot',
+      method: 'GET',
+      headers: {
+        "Authorization" : "Basic L29yZy0xL2FkbWluOkNoYW5nM20zISEtYWRtaW4tb3JnLTE=",
+        "nep-application-key" : "8a82859f5ef21870015ef2fa5e5f0000",
+        "Content-Type": "application/json",
+      }
 
 
-	};
+    };
 
-	var tmp = https.get(options, function(res) {
-		res.on('data', function (chunk) {
+    var tmp = https.get(options, function(res) {
+        res.on('data', function (chunk) {
             output += chunk;
         });
 
@@ -44,15 +45,49 @@ app.get('/', function (req, resOrigin) {
             var obj = JSON.parse(output);
 
             resOrigin.render('index.ejs', {
-            	data: obj.snapshot
+                data: obj.snapshot
             })
         });
-	});
+    });
 
 
 
-  
 })
+
+
+  app.get('/cart', function (req, resOrigin) {
+    var output = "";
+    var options = {
+      host: 'hackgt-api.ncrcloud.com',
+      port: 443,
+      path: '/catalog/items/snapshot',
+      method: 'GET',
+      headers: {
+        "Authorization" : "Basic L29yZy0xL2FkbWluOkNoYW5nM20zISEtYWRtaW4tb3JnLTE=",
+        "nep-application-key" : "8a82859f5ef21870015ef2fa5e5f0000",
+        "Content-Type": "application/json",
+      }
+
+
+    };
+
+    var tmp = https.get(options, function(res) {
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            var obj = JSON.parse(output);
+
+            resOrigin.render('cart.ejs', {
+                data: obj.snapshot
+            })
+        });
+    });
+})
+
+
+
 /*
 app.get('/item', function (req, resOrigin) {
 	const itemID = req.query.itemID;
@@ -100,14 +135,12 @@ app.get('/url', function (req, resOrigin) {
 	request(url, function (error, response, html) {
 		
 	  if (!error && response.statusCode == 200) {
-
-	    
-	      
 	  	var data;
 		if(url.includes("familycircle") ){
 			data = getFamilyCircle(html);
 		}
 		resOrigin.send(JSON.stringify(data));
+        // return resOrigin.redirect("/cart.ejs");
 	  }
 	});
 
