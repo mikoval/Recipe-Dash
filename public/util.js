b9edcd5cec06bb4333b26158896161535ea8ff72
Kanan <kananpatel@gatech.edu>
2017-10-15 00:33:18 -0400

    updated algorithm for matching ingredients more efficiently

--- public/util.js
+++ public/util.js
@@ -14,13 +14,57 @@ function addIngredients(input){
 	console.log(ingredients);
 }
 function convertName(name){
+	var minLength = 99999999999;
+	var minIndex = undefined;
+	var maxMatches = 0;
+
 	for(var i = 0; i < data.length; i++){
 		var dataName = data[i].shortDescription.values[0].value;
 		if(dataName.toLowerCase().includes(name.toLowerCase())){
-			return dataName;
+			if (dataName.length < minLength) {
+				minLength = dataName.length;
+				minIndex = i;
+			}
+	
 		}
+
 	}
-	return "";
+	if (minIndex != undefined) {
+		return data[minIndex].shortDescription.values[0].value;
+	} else {
+		var array = name.split(" ");
+		for (var i = 0; i < data.length; i++){
+			var dataName = data[i].shortDescription.values[0].value;
+			var count = 0;
+
+			for (var j = 0; j < array.length; j++){
+				var subStr = array[j];
+
+				if(dataName.toLowerCase().includes(subStr.toLowerCase())){
+					count++;
+	
+				}
+			}
+			if (count == maxMatches) {
+				if (dataName.length < minLength) {
+					maxMatches = count;
+					minLength = dataName.length;
+					minIndex = i;
+				}
+
+			} 
+			else if (count > maxMatches) {
+				maxMatches = count;
+				minLength = dataName.length;
+				minIndex = i;
+
+			}
+
+		}
+
+		return data[minIndex].shortDescription.values[0].value;
+	}
+	/*return "";*/
 
 
 }
