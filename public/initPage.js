8a207ee3872cb5580073d2b5fa999243dbd10e16
Mike <mkoval3@gatech.edu>
2017-10-15 02:38:13 -0400

    set background

--- public/initPage.js
+++ public/initPage.js
@@ -5,7 +5,9 @@ $(document).ready(function () {
     });
 
     $("#searchMore-btn").on("click", function(){
+    	console.log("click");
         var url = "/url?url="+ $("#searchMore").val();
+        $("#searchMore").val("");
         $.ajax({
             url: url,
             success: function(response){
@@ -14,7 +16,22 @@ $(document).ready(function () {
             }
         })
     })
-    console.log(window.location.pathname.split("?"));
+    var url = getParameterByName('url');
+    if(url.length > 0);
+    $("#searchMore").val(url);
+    $("#searchMore-btn").trigger("click");
+
 
 });
+
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
 
